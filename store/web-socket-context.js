771480acce07008be432a8cd3616cf6ceb6b35c1
Joel Velis <joelvelis94@gmail.com>
2021-10-08 10:27:04 -0400

    set up notifications specific to each user involved in bet

--- store/web-socket-context.js
+++ store/web-socket-context.js
@@ -1,47 +1,11 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useCookies } from 'react-cookie';
-import dynamic from 'next/dynamic'
-
-
-
-// const ActioncableNoSSR = dynamic(
-//   () => import('actioncable').then((mod) => mod.ActionCable),
-//   { ssr: false }
-// )
-
 
 const WebSocketContext = React.createContext({
   isPaused: false,
 });
 
 export const WebSocketContextProvider = (props) => {
-    // const [notifications, setNotifications] = useState()
-    // const [cookies, setCookie, removeCookie] = useCookies([]);
-    // const [cable, setCable] = useState()
-    // const [isPaused, setPause] = useState(false);
-    
-    
-    // useEffect(() => {
-    //     const ActionCable = require('actioncable');
-    //     const cable = ActionCable.createConsumer(`ws://localhost:3000/cable?user_id=${cookies.user_id}`)
-    //     cable.subscriptions.create(
-    //         { channel: 'ApplicationCable::NotificationChannel' },
-    //         { received: message => setNotifications(message) }
-    //     )
-    //     console.log(cable)
-    // })
-
-    
-        
-
-    // useEffect(() => {
-    //     cable.subscriptions.create(
-    //         { channel: 'NotificationsChannel' },
-    //         { received: message => setNotifications(message) }
-    //     )
-    // })
-
-    
     const [isPaused, setPause] = useState(false);
     const ws = useRef(null);
     const [cookies, setCookie, removeCookie] = useCookies([]);
@@ -50,8 +14,8 @@ export const WebSocketContextProvider = (props) => {
         ws.current = new WebSocket(`ws://localhost:3000/cable?user_id=${cookies.user_id}`);
         ws.current.onopen = () => {
             console.log("ws opened");
-            ws.current.send(JSON.stringify({"command": "subscribe","identifier":"{\"channel\":\"NotificationsChannel\"}"}))
-            // ws.current.send(JSON.stringify({"command": "subscribe","identifier":"{\"channel\":\"notifications_channel\"}"}))
+            ws.current.send(JSON.stringify({"command": "subscribe","identifier":"{\"channel\":\"NotificationsChannel" + "\"}"}))
+            
         }
         ws.current.onclose = () => console.log("ws closed");
 
@@ -69,6 +33,8 @@ export const WebSocketContextProvider = (props) => {
             if (isPaused) return;
             const message = JSON.parse(e.data);
             console.log("e", message);
+            console.log(`recipient is ${message?.message?.recipient_id}`)
+            // console.log(message.message)
         };
     }, [isPaused]);
 
